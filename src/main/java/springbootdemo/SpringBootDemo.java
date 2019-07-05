474b9c39fa1f306e8ea6784e7dfa6c45bbb6705a
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-07-05 16:08:28 +0800

    Building a RESTful Web Service

--- src/main/java/springbootdemo/SpringBootDemo.java
+++ src/main/java/springbootdemo/SpringBootDemo.java
@@ -1,23 +1,21 @@
 package springbootdemo;
-import org.springframework.boot.*;
+
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
 @EnableAutoConfiguration
+@SpringBootApplication
+
 public class SpringBootDemo {
 
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
 		return "Welcome!";
-	}
-	
-	@RequestMapping("/about")
-	@ResponseBody
-	String about() {
-		return "This is about Page for you";
 	}
 	
 	public static void main(String[] args) throws Exception {
