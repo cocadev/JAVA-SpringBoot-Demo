a48edfbfae56f52b91f57b2f5b14807dcdfd8877
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-07-06 00:10:33 +0800

    consuming-rest with log

--- src/main/java/springbootdemo/SpringBootDemo.java
+++ src/main/java/springbootdemo/SpringBootDemo.java
@@ -1,26 +1,40 @@
 package springbootdemo;
 
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+//import org.springframework.boot.autoconfigure.*;
+//import org.springframework.stereotype.*;
+//import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.joda.time.LocalTime;
+//import org.joda.time.LocalTime;
 
-@Controller
-@EnableAutoConfiguration
-@SpringBootApplication
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+//@Controller
+//@EnableAutoConfiguration
+//@SpringBootApplication
 
 public class SpringBootDemo {
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		LocalTime currentTime = new LocalTime();
-		return "Welcome!" + currentTime;
-	}
+//	@RequestMapping("/")
+//	@ResponseBody
+//	String home() {
+//		LocalTime currentTime = new LocalTime();
+//		return "Welcome!" + currentTime;
+//	}
+	
+    private static final Logger log = LoggerFactory.getLogger(SpringBootDemo.class);
+
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootDemo.class, args);
+		
+		RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+        
+//		SpringApplication.run(SpringBootDemo.class, args);
 	}
 }
---
+++ src/main/java/springbootdemo/WebController.java
@@ -0,0 +1,34 @@
+package springbootdemo;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RestController
+public class WebController {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
